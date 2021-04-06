ef02c3ee29d3e0b897a5ddf514240242864c15b3
LukeHarjulin <60152401+LukeHarjulin@users.noreply.github.com>
2021-04-06 17:35:04 +0100

    Updated VNet testing

--- src/Infrastructure/Stize.Infrastructure.Azure.Tests/Networking/VNetTests.cs
+++ src/Infrastructure/Stize.Infrastructure.Azure.Tests/Networking/VNetTests.cs
@@ -1,8 +1,12 @@
 
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using FluentAssertions;
+using Pulumi;
 using Pulumi.AzureNative.Network;
+using Pulumi.Testing;
+using Stize.Infrastructure.Azure.Tests.Networking.Stacks;
 using Stize.Infrastructure.Tests.Azure.Networking.Stacks;
 using Xunit;
 
@@ -11,15 +15,23 @@ namespace Stize.Infrastructure.Tests.Azure.Networking
     public class VNetTests
     {
         [Fact]
-        public async System.Threading.Tasks.Task CreateBasicVnet()
+        public async Task CreateBasicVnet()
         {
-            
-            var resources = await Stize.Infrastructure.Test.Testing.RunAsync<NetworkingBasicStack>();
+            var resources = await Deployment.TestAsync<VNetBasicStack>(new VNetBasicMock(), new TestOptions { IsPreview = false });
             var vnet = resources.OfType<VirtualNetwork>().FirstOrDefault();
 
             vnet.Should().NotBeNull("Virtual Network not found");
             vnet.Name.Apply(x => x.Should().Be("vnet1"));
             vnet.Location.Apply(x => x.Should().Be("westeurope"));
+        }
+
+        [Fact]
+        public async Task AddressSpacesAreCorrect()
+        {
+            var resources = await Deployment.TestAsync<VNetBasicStack>(new VNetBasicMock(), new TestOptions { IsPreview = false });
+            var vnet = resources.OfType<VirtualNetwork>().FirstOrDefault();
+
+            (await vnet.AddressSpace.GetValueAsync())?.AddressPrefixes.Should().Contain("172.16.0.0/24");
         }
     }
 }
