49f88338391dfcf7f10235c4cc68224e49d91cd0
LukeHarjulin <60152401+LukeHarjulin@users.noreply.github.com>
2021-05-06 17:05:58 +0100

    Added Agent Pool buil'der/extensions
    Not fully sure about this builder. Might be reverted, as there might be a better way to add agent pools to the managed cluster.

---
+++ src/Infrastructure/Stize.Infrastructure.Azure/ContainerService/AgentPoolBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pulumi.AzureNative.ContainerService.Inputs;
+namespace Stize.Infrastructure.Azure.ContainerService
+{
+    public class AgentPoolBuilder
+    {
+
+        public ManagedClusterAgentPoolProfileArgs Arguments = new ManagedClusterAgentPoolProfileArgs()
+        {
+            VmSize = "Standard_Ds2_v2",
+            Name = "agentpool",
+            Count =  3,
+            Mode = "System",
+            MaxPods = 110,
+            OsDiskType = "Managed",
+            OsDiskSizeGB = 128,
+            OsType = "Linux",
+            Type = "VirtualMachineScaleSets",
+            AvailabilityZones = "None"   
+        };
+        public AgentPoolBuilder()
+        {
+
+        }
+
+        public ManagedClusterAgentPoolProfileArgs Build()
+        {
+            return Arguments;
+        }
+    }
+}
---
+++ src/Infrastructure/Stize.Infrastructure.Azure/ContainerService/AgentPoolExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pulumi;
+using Pulumi.AzureNative.ContainerService;
+using Pulumi.AzureNative.ContainerService.Inputs;
+namespace Stize.Infrastructure.Azure.ContainerService
+{
+    public static class AgentPoolExtensions
+    {
+        public static AgentPoolBuilder Name(this AgentPoolBuilder builder, Input<string> name)
+        {
+            builder.Arguments.Name = name;
+            return builder;
+        }
+
+        public static AgentPoolBuilder Count(this AgentPoolBuilder builder, Input<int> count)
+        {
+            builder.Arguments.Count = count;
+            return builder;
+        }
+
+        public static AgentPoolBuilder Mode(this AgentPoolBuilder builder, InputUnion<string, AgentPoolMode> mode)
+        {
+            builder.Arguments.Mode = mode;
+            return builder;
+        }
+
+        public static AgentPoolBuilder OsType(this AgentPoolBuilder builder, InputUnion<string, OSType> osType)
+        {
+            builder.Arguments.OsType = osType;
+            return builder;
+        }
+
+        public static AgentPoolBuilder Type(this AgentPoolBuilder builder, InputUnion<string, AgentPoolType> type)
+        {
+            builder.Arguments.Type = type;
+            return builder;
+        }
+
+        public static AgentPoolBuilder Type(this AgentPoolBuilder builder, Input<string> size)
+        {
+            builder.Arguments.VmSize = size;
+            return builder;
+        }
+    }
+}
